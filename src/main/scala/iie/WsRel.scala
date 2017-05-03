08409c5c22f6e5cb0f5ee79445fd306f1252b668
Ransford <dingyu.sdu@gmail.com>
2017-05-03 20:55:58 +0800

    add calc path WsRel weight sum

--- src/main/scala/iie/WsRel.scala
+++ src/main/scala/iie/WsRel.scala
@@ -15,21 +15,25 @@ object WsRel {
 
     val u2m = hc.sql("select * from rans.u2m")
     val m2a = hc.sql("select * from rans.m2a")
+    val m2d = hc.sql("select * from rans.m2d")
+
+    val s = "61674"
+    val t = "364"
 
     // U-M-A-M的WsRel
-    val Wumam = calcWsRel("u1", "m2", u2m, m2a, hc)
-    val m2d = hc.sql("select * from rans.m2d")
+    val Wumam = calcWsRel_m2a(s, t, u2m, m2a, hc)
+
     // U-M-D-M的WsRel
-    val Wumdm = calcWsRel("61674", "377", u2m, m2d, hc)
+    val Wumdm = calcWsRel_m2d(s, t, u2m, m2d, hc)
 
     val Wpi = calcPathWeight(u2m, m2a, m2d, hc)
 
     // 计算两个路径加权后的值（现在有bug）
-    //  val result = Wumam*Wpi._1.toDouble + Wumam*Wpi._2.toDouble
+    val result = Wumam * Wpi._1 + Wumdm * Wpi._2
 
   }
 
-  def calcWsRel(s: String, t: String, u2m: DataFrame, m2a: DataFrame, hc: HiveContext) = {
+  def calcWsRel_m2a(s: String, t: String, u2m: DataFrame, m2a: DataFrame, hc: HiveContext) = {
     // 从《算法思想》文档中WsRel（U1,M2）|R1R2R3) 推导公式的最后一步往上进行计算
     // 先计算[ 1/3【w(M1,a1)*WsRel(a1,M2)+w(M1,a2)*WsRel(a2,M2)+ w(M1,a3)*WsRel(a3,M2)]】+。。。，这里的【】中的和其实就是：
     //    1.	是路径U-M-A-M' 上   M-A的权重的和再乘以M的出度分之一
@@ -37,7 +41,7 @@ object WsRel {
     // ---------------------------
     // 接下来开始计算
     // 计算从s点出发涉及到的所有movie
-    val movies = u2m.filter(u2m("user").equalTo("u1")).select("movie").distinct()
+    val movies = u2m.filter(u2m("user").equalTo(s)).select("movie").distinct()
     // 计算公式最后是过滤涉及到的M-A边，设(s,t)间路线为U-M-A-M'，这里求的就是M-A和A-M',这里的A-M'路径对应WsRel表达式求解最后一步的A-M'(t)是否通联,M-A包括A不与M'通联的情况
     // 注意这里的join条件本应该写为‘m2a.join(movies, m2a("movie") === movies("movie"), "Left")’，但是这么写会产生4列，所以才使用了Seq的这个方式
     val conPathMA = movies.join(m2a, Seq("movie"), "Left")
@@ -63,15 +67,43 @@ object WsRel {
 
     // 这里是对U-M这一步进行计算
     // 计算1/|O(U1|R1),核心为计算出度
-    val WsRel_um_out_degree = 1.0 / u2m.filter(u2m("user").equalTo("u1")).count()
+    val WsRel_um_out_degree = 1.0 / u2m.filter(u2m("user").equalTo(s)).count()
     // 下面是计算U-M-A-M'的U-M边
     // 先计算M-A-M'种所有的M集合，然后基于M集合和U，来计算有效的U-M集合（有效是指，U-M为s-t的一部分）
     val conMovie = M_A.select("movie").distinct()
-    val conUM = conMovie.join(u2m.filter(u2m("user").equalTo("u1")), Seq("movie"), "Left").select("user", "movie", "weight")
+    val conUM = conMovie.join(u2m.filter(u2m("user").equalTo(s)), Seq("movie"), "Left").select("user", "movie", "weight")
 
     val wu1_s = WsResl_w_MAMt.join(conUM, Seq("movie"), "Left")
     val result_WsResl = wu1_s.select(wu1_s.col("wma_am").*(wu1_s.col("weight"))).withColumnRenamed("(wma_am * weight)", "wmam_w")
-    val result = result_WsResl.select(sum("wmam_w").*(WsRel_um_out_degree)).first().
+    val resultTmp = result_WsResl.select(sum("wmam_w").*(WsRel_um_out_degree)).first().get(0)
+    var stWsRel_m2a = 0.0
+    if (resultTmp != null)
+      stWsRel_m2a = resultTmp.toString.toDouble
+    stWsRel_m2a
+  }
+
+  // 这里之所以要把m2a与m2d的计算WsRel的方法分开，是因为：方法里有些地方指定了表名、指定了表内的字段名，而m2a与m2d两个表的表名与字段名有区别
+  def calcWsRel_m2d(s: String, t: String, u2m: DataFrame, m2d: DataFrame, hc: HiveContext) = {
+    val movies = u2m.filter(u2m("user").equalTo(s)).select("movie").distinct()
+    val conPathMD = movies.join(m2d, Seq("movie"), "Left")
+    val tDirectors = hc.sql("select distinct director from rans.m2d where movie = " + "'" + t + "'")
+    val M_A_Mt = tDirectors.join(conPathMD, Seq("director"), "Left")
+    val A_Mt = M_A_Mt.filter(M_A_Mt("movie").equalTo(t))
+    val M_A = M_A_Mt.filter(M_A_Mt("movie").notEqual(t))
+    val m2d_out_degree_1cent = hc.sql("select movie,1.0/count(*) as degree from rans.m2d group by movie")
+    val sum_w = M_A.groupBy(M_A("movie")).sum("weight").withColumnRenamed("sum(weight)", "wsum")
+    val tmp_sumw_1cent = sum_w.join(m2d_out_degree_1cent, Seq("movie"), "Left")
+    val WsResl_w_MAMt = tmp_sumw_1cent.select(tmp_sumw_1cent.col("movie"), tmp_sumw_1cent.col("wsum").*(tmp_sumw_1cent.col("degree"))).withColumnRenamed("(wsum * degree)", "wma_am")
+    val WsRel_um_out_degree = 1.0 / u2m.filter(u2m("user").equalTo(s)).count()
+    val conMovie = M_A.select("movie").distinct()
+    val conUM = conMovie.join(u2m.filter(u2m("user").equalTo(s)), Seq("movie"), "Left").select("user", "movie", "weight")
+    val wu1_s = WsResl_w_MAMt.join(conUM, Seq("movie"), "Left")
+    val result_WsResl = wu1_s.select(wu1_s.col("wma_am").*(wu1_s.col("weight"))).withColumnRenamed("(wma_am * weight)", "wmam_w")
+    val resultTmp = result_WsResl.select(sum("wmam_w").*(WsRel_um_out_degree)).first().get(0)
+    var stWsRel_m2d = 0.0
+    if (resultTmp != null)
+      stWsRel_m2d = resultTmp.toString.toDouble
+    stWsRel_m2d
   }
 
   def calcPathWeight(u2m: DataFrame, m2a: DataFrame, m2d: DataFrame, hc: HiveContext) = {
@@ -136,7 +168,7 @@ object WsRel {
     val m2a = hc.sql("select * from rans.m2a")
     val s = "61674"
     val t = "364"
-    val movies = u2m.filter(u2m("user").equalTo("u1")).select("movie").distinct()
+    val movies = u2m.filter(u2m("user").equalTo(s)).select("movie").distinct()
     val conPathMA = movies.join(m2a, Seq("movie"), "Left")
     val tActors = hc.sql("select distinct actor from rans.m2a where movie = " + "'" + t + "'")
     val M_A_Mt = tActors.join(conPathMA, Seq("actor"), "Left")
@@ -146,9 +178,9 @@ object WsRel {
     val sum_w = M_A.groupBy(M_A("movie")).sum("weight").withColumnRenamed("sum(weight)","wsum")
     val tmp_sumw_1cent = sum_w.join(m2a_out_degree_1cent, Seq("movie"), "Left")
     val WsResl_w_MAMt = tmp_sumw_1cent.select(tmp_sumw_1cent.col("movie"),tmp_sumw_1cent.col("wsum").*(tmp_sumw_1cent.col("degree"))).withColumnRenamed("(wsum * degree)","wma_am")
-    val WsRel_um_out_degree = 1.0 / u2m.filter(u2m("user").equalTo("u1")).count()
+    val WsRel_um_out_degree = 1.0 / u2m.filter(u2m("user").equalTo(s)).count()
     val conMovie = M_A.select("movie").distinct()
-    val conUM = conMovie.join(u2m.filter(u2m("user").equalTo("u1")),Seq("movie"),"Left").select("user","movie","weight")
+    val conUM = conMovie.join(u2m.filter(u2m("user").equalTo(s)),Seq("movie"),"Left").select("user","movie","weight")
     val wu1_s = WsResl_w_MAMt.join(conUM,Seq("movie"),"Left")
     val result_WsResl = wu1_s.select(wu1_s.col("wma_am").*(wu1_s.col("weight"))).withColumnRenamed("(wma_am * weight)","wmam_w")
     val result = result_WsResl.select(sum("wmam_w").*(WsRel_um_out_degree)).show
